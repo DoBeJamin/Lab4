a9ddc9f961a8990414d863756530e820cef8b2f0
DoBeJamin <benjaminick11@gmail.com>
2023-06-28 12:58:26 -0500

    updated to use a normal led not multi color led

--- src/Lab4.cpp
+++ src/Lab4.cpp
@@ -4,40 +4,25 @@
 
 #include "Particle.h"
 #line 1 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab4/src/Lab4.ino"
+
 void setup();
 void loop();
-#line 1 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab4/src/Lab4.ino"
+#line 2 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab4/src/Lab4.ino"
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
-
-
-
-
-
-
 
 void setup() {
 
   //initialize pins
-  pinMode(D7, OUTPUT);
-  pinMode(D6, OUTPUT);
   pinMode(D3, OUTPUT);
 
   pinMode(D8, INPUT);
-
-
+  
 }
 
 void loop() {
 
-  if (digitalRead(D8)) {
-    analogWrite(D7,0); 
-    analogWrite(D6,0); 
-    analogWrite(D3,0); 
-  } else {
-    analogWrite(D7,255); 
-    analogWrite(D6,255); 
-    analogWrite(D3,255); 
-  }
+  // Light on and off based on button
+  digitalWrite(D3, digitalRead(D8));
 
 }
--- src/Lab4.ino
+++ src/Lab4.ino
@@ -5,24 +5,15 @@ SYSTEM_THREAD(ENABLED);
 void setup() {
 
   //initialize pins
-  pinMode(D7, OUTPUT);
-  pinMode(D6, OUTPUT);
   pinMode(D3, OUTPUT);
 
   pinMode(D8, INPUT);
+  
 }
 
 void loop() {
 
   // Light on and off based on button
-  if (digitalRead(D8)) {
-    analogWrite(D7,0); 
-    analogWrite(D6,0); 
-    analogWrite(D3,0); 
-  } else {
-    analogWrite(D7,255); 
-    analogWrite(D6,255); 
-    analogWrite(D3,255); 
-  }
+  digitalWrite(D3, digitalRead(D8));
 
 }
